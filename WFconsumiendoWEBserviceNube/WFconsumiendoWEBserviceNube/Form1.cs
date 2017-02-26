d08339fee475850c0fdc05ea1c389d97879a02b8
AlexanderQuinaluiza <alexanderquinaluiza2b@gmail.com>
2017-02-26 15:54:20 -0500

    Add files via upload

---
+++ WFconsumiendoWEBserviceNube/WFconsumiendoWEBserviceNube/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFconsumiendoWEBserviceNube
+{
+
+   
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// solo se toma en cuenta la referencia al webservice
+        /// </summary>
+        dniCal.Calculator cal = new dniCal.Calculator();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            label1.Text= "Add "+ cal.Add(100,5)+"mutiplicaccion "+ cal.Multiply(7, 9)
+                
+                + " division " + cal.Divide(100, 5) + "Subtract " + cal.Subtract(7, 9);
+
+
+
+
+
+        }
+    }
+}
